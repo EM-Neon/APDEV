403dd07fe0d37fd14b7e42ea823f7860dab81402
EM-Neon <blindsuneclipse@gmail.com>
2021-08-13 03:43:29 +0800

    Edited test scene, edited enemy script, added test materials for enemy types

--- APDEV/Assets/Scripts/Enemy.cs
+++ APDEV/Assets/Scripts/Enemy.cs
@@ -1,18 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
+    enum EnemyType {Power, Speed, Regular};
+    private EnemyType myType;
+    private float[] leftOrRight = { 1, -1 };
+
+    [SerializeField] private Material[] typeColors = new Material[3];
+    [SerializeField] private float[] shootSpeed = new float[3];
+    [SerializeField] private float[] moveSpeed = new float[3];
+
+    float moveTick = 0;
+    float moveDelay;
+    float moveDelay2;
+    int randomLeftOrRight;
+
+    float shootTick = 0;
+    float shootDelay;
+    float shootDelay2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine("MovementTimer");
+        //StartCoroutine("ShootTimer");
+
+        int type = Random.Range(0, 3);
+        myType = (EnemyType)type;
+        gameObject.GetComponent<MeshRenderer>().material = typeColors[type];
+
+        Debug.Log(myType);
+
+        moveDelay = Random.Range(0.5f, 1.0f);
+        moveDelay2 = Random.Range(0.25f, 0.5f) + moveDelay;
+        randomLeftOrRight = Random.Range(0, 2);
+
+        shootDelay = Random.Range(0.5f, 1.0f);
+        shootDelay2 = Random.Range(0.5f, 1.0f);
+    }
+    private IEnumerator MovementTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+
+            // move left or right
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(leftOrRight[Random.Range(0, 2)] * (3 * moveSpeed[(int)myType]), 0, 0);
+
+            yield return new WaitForSeconds(100);
+
+            // stop moving
+
+            //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    private IEnumerator ShootTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+
+            //start shoot timer
+            Debug.Log(gameObject.name + "is Gonna throw!");
+            this.transform.GetChild(0).gameObject.SetActive(true);
+
+            this.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount -= 1/shootSpeed[(int)myType];
+
+            yield return new WaitForSeconds(shootSpeed[(int)myType]);
+
+            //get shot
+            Debug.Log("Got shot by" + gameObject.name + "!");
+
+            this.transform.GetChild(0).gameObject.SetActive(false);
+            this.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = 1;
+            //Cooldown
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveTick += Time.deltaTime;
+        if (moveTick >= moveDelay)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(leftOrRight[randomLeftOrRight] * (3 * moveSpeed[(int)myType]), 0, 0);
+            if (moveTick >= moveDelay2)
+            {
+                gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                moveTick = 0;
+                moveDelay = Random.Range(0.5f, 1.0f);
+                moveDelay2 = Random.Range(0.25f, 0.5f) + moveDelay;
+                randomLeftOrRight = Random.Range(0, 2);
+            }
+        }
+
+        shootTick += Time.deltaTime;
+        if(shootTick >= shootDelay)
+        {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            this.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount -= 1.0f / shootSpeed[(int)myType] * Time.deltaTime;
+            if(shootTick >= shootDelay + shootSpeed[(int)myType])
+            {
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                this.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = 1;
+                if(shootTick >= shootDelay + shootSpeed[(int)myType] + shootDelay2)
+                {
+                    shootTick = 0;
+                    shootDelay = Random.Range(0.5f, 1.0f);
+                    shootDelay2 = Random.Range(0.5f, 1.0f);
+                }
+            }
+        }
     }
 }
