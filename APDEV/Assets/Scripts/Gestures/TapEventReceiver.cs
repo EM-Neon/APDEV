84cca452ce66538c823778f9ed36ede1fcc4b3df
CesarManalac <cesar_manalac@dlsu.edu.ph>
2021-08-13 08:04:14 +0800

    updated scripts with touch input

--- APDEV/Assets/Scripts/Gestures/TapEventReceiver.cs
+++ APDEV/Assets/Scripts/Gestures/TapEventReceiver.cs
@@ -29,15 +29,13 @@ public class TapEventReceiver : MonoBehaviour
     public void SpawnObject(Vector3 pos)
     {
         GameObject spawn = GameObject.Instantiate(spawnItem, transform);
-        spawn.transform.position = Camera.main.ScreenToWorldPoint(pos);
-        Debug.Log($"Position {pos}");
+        spawn.transform.localPosition = Vector3.zero;
         Ray screenToPointer = Camera.main.ScreenPointToRay(pos);
         RaycastHit hit;
         if (Physics.Raycast(screenToPointer, out hit, Mathf.Infinity))
         {
-            /*Vector3 theVector = (transform.position - hit.point).normalized;*/
-            Vector3 theVector = screenToPointer.direction;
-            /*spawn.GetComponent<Rigidbody>().velocity = new Vector3(theVector.x * -60, theVector.y * -60, theVector.z * -60);*/
+            Vector3 theVector = (transform.position - hit.point).normalized;
+            spawn.GetComponent<Rigidbody>().velocity = new Vector3(theVector.x * -60, theVector.y * -60, theVector.z * -60);
         }
     }
 }
